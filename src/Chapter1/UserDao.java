d0febc4085fa6056725298a36959658efae694b1
Kwon Young Jae <kyoje11@gmail.com>
2018-07-25 14:04:13 +0900

    Nesoy : Update Spring DAO

---
+++ src/Chapter1/UserDao.java
@@ -0,0 +1,9 @@
+package Chapter1;
+
+public class UserDao {
+	// DB 연결을 위한 Connection을 가져온다
+	// SQL을 담은 Statement, PreparedStatement를 만든다
+	// 실행 후 결과를 ResultSet으로 받는다
+	// Connect, Statement, ResultSet를 닫아준다
+	// 실행 중 발생한 Exception을 잡아서 직접 처리하거나 throws로 던진다
+}
